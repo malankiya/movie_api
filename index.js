0ddde950b6c9f0895620db4e58b95d828cd13ab6
Mittal-Malankiya <m4mittal19@gmail.com>
2024-02-06 16:56:23 +0100

    install nodemon

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-
 const app = express();
+const bodyParser = require("body-parser");
+uuid = require("uuid");
+
+app.use(bodyParser.json());
+// Define an array with users
+let users = [
+  (id = "1"),
+  (Name = "joe"),
+  (Favouritemovie = []),
+
+  (id = "2"),
+  (Name = "joni"),
+  (Favouritemovie = ["Pulp Fiction"]),
+];
 // Define an array with data about the top 10 movies
 const topMovies = [
   {
@@ -90,12 +101,11 @@ app.get("/", (req, res) => {
 
 // Gets the data about a single movie, by title
 app.get("/movies/:title", (req, res) => {
-  const requestedMovie = movies.find(
-    (movie) => movie.title === req.params.title
-  );
+  const { title } = req.params;
+  const movie = movies.find((movie) => movie.title === title);
 
-  if (requestedMovie) {
-    res.json(requestedMovie);
+  if (movie) {
+    res.status(200).json(movie);
   } else {
     res.status(404).send("Movie not found");
   }
