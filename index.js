65d2b372efa2606bd2c104dbc791b83fb5a64cba
Mittal-Malankiya <m4mittal19@gmail.com>
2024-02-15 13:55:12 +0100

    update index.js

--- index.js
+++ index.js
@@ -17,6 +17,89 @@ mongoose
 
 // Import Mongoose models
 const User = Models.User;
+const Movie = Models.Movie;
+
+// Get all Movies from the database
+app.get("/movies", async (req, res) => {
+  try {
+    const movies = await Movie.find({});
+    res.json(movies);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+// get single movie by movieName
+app.get("/movies/:movieName", async (req, res) => {
+  const { movieName } = req.params;
+
+  try {
+    const movie = await Movie.findOne({ movieName: movieName });
+
+    if (movie) {
+      const movieData = {
+        genre: movie.genre,
+        director: movie.director,
+        movieid: movie.movieid,
+        birthdate: movie.birthdate,
+        description: movie.description,
+        movieName: movie.movieName,
+        bio: movie.bio,
+      };
+
+      res.json(movieData);
+    } else {
+      res.status(404).send("Movie not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+// ger description by genreName
+app.get("/movies/genre/:genreName", async (req, res) => {
+  const { genreName } = req.params;
+
+  try {
+    const movie = await Movie.findOne(
+      { genre: genreName },
+      "genre description"
+    );
+
+    if (movie) {
+      res.json({ genre: movie.genre, description: movie.description });
+    } else {
+      res.status(404).send("Genre not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+// Return data about a director
+app.get("/directors/:directorName", async (req, res) => {
+  const { directorName } = req.params;
+
+  try {
+    const movie = await Movie.findOne(
+      { director: directorName },
+      "director birthdate bio"
+    );
+
+    if (movie) {
+      res.json({
+        director: movie.director,
+        birthdate: movie.birthdate,
+        bio: movie.bio,
+      });
+    } else {
+      res.status(404).send("Director not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 // Get all users from the database
 app.get("/users", async (req, res) => {
@@ -70,7 +153,7 @@ app.get("/users/:firstName", async (req, res) => {
       res.status(500).send("Error: " + err);
     });
 });
-
+// Update
 app.put("/users/:_id", async (req, res) => {
   try {
     const updatedUser = await User.findOneAndUpdate(
@@ -130,124 +213,6 @@ app.delete("/users/:firstName", async (req, res) => {
     res.status(500).send("Error: " + err);
   }
 });
-
-// // Allow new users to register;
-
-// app.post("/users", (req, res) => {
-//   const newUser = req.body;
-
-//   if (newUser.name) {
-//     newUser.id = uuid.v4();
-//     users.push(newUser);
-//     res.status(201).json(newUser);
-//   } else {
-//     res.status(400).send("users need names");
-//   }
-// });
-
-// // update the data
-// app.put("/users/:id", (req, res) => {
-//   const { id } = req.params;
-//   const updatedUser = req.body;
-
-//   let user = users.find((user) => user.id == id);
-
-//   if (user) {
-//     user.name = updatedUser.name;
-//     res.status(200).json(user);
-//   } else {
-//     res.status(400).send("such user not found");
-//   }
-// });
-
-// //Allow users to add a movie to their list of favorites
-// app.post("/users/:id/:movieTitle", (req, res) => {
-//   const { id, movieTitle } = req.params;
-
-//   let user = users.find((user) => user.id == id);
-
-//   if (user) {
-//     user.Favouritemovie.push(movieTitle); // Update the property name here
-//     res.status(200).send(`${movieTitle} has been added to user  ${id}'s array`);
-//   } else {
-//     res.status(400).send("user not found");
-//   }
-// });
-
-// //DELETE
-// app.delete("/users/:id/:movieTitle", (req, res) => {
-//   const { id, movieTitle } = req.params;
-
-//   let user = users.find((user) => user.id == id);
-
-//   if (user) {
-//     user.Favouritemovie = user.Favouritemovie.filter(
-//       (title) => title !== movieTitle
-//     );
-//     res
-//       .status(200)
-//       .send(`${movieTitle} has been remove from user  ${id}'s array`);
-//   } else {
-//     res.status(400).send("user not found");
-//   }
-// });
-
-// //Allow existing users to deregister
-// app.delete("/users/:id", (req, res) => {
-//   const { id } = req.params;
-
-//   let user = users.find((user) => user.id == id);
-
-//   if (user) {
-//     users = users.filter((user) => user.id != id);
-//     res.send(`user${id} has been deleted`);
-//   } else {
-//     res.status(400).send("user not found");
-//   }
-// });
-
-// // Create an Express GET route at the endpoint "/movies"
-// app.get("/movies", (req, res) => {
-//   res.json(topMovies);
-// });
-
-// // Gets the data about a single movie, by title
-// app.get("/movies/:title", (req, res) => {
-//   const { title } = req.params;
-//   const movie = topMovies.find((movie) => movie.title === title);
-
-//   if (movie) {
-//     res.status(200).json(movie);
-//   } else {
-//     res.status(400).send("Movie not found");
-//   }
-// });
-
-// // Gets the data about a genre, by genrename
-// app.get("/movies/genre/:genreName", (req, res) => {
-//   const { genreName } = req.params;
-//   const genre = topMovies.find((movie) => movie.genre.Name === genreName).genre;
-
-//   if (genre) {
-//     res.status(200).json(genre);
-//   } else {
-//     res.status(400).send(" not found genre");
-//   }
-// });
-
-// // Return data about a genre (description)
-// app.get("/movies/directors/:directorName", (req, res) => {
-//   const { directorName } = req.params;
-//   const director = topMovies.find(
-//     (movie) => movie.Director.Name === directorName
-//   ).Director;
-
-//   if (director) {
-//     res.status(200).json(director);
-//   } else {
-//     res.status(400).send(" not found director");
-//   }
-// });
 
 // // // Error-handling middleware
 app.use((err, req, res, next) => {
