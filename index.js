97b5ffcf59bf532109e95b578a4cb80b39ac6ef5
Mittal-Malankiya <m4mittal19@gmail.com>
2024-02-20 10:15:56 +0100

    update index.js

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const app = express();
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
+
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "http://localhost:8000",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 //Import auth.js
 let auth = require("./auth")(app);
 
@@ -217,6 +240,7 @@ app.get(
 // CREATE
 app.post("/users", async (req, res) => {
   try {
+    let hashedPassword = User.hashPassword(req.body.password);
     const existingUser = await User.findOne({ userName: req.body.userName });
 
     if (existingUser) {
