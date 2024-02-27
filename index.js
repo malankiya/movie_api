c0355d2a5034356b9d862ba8bc88ea1185ad8954
Mittal-Malankiya <m4mittal19@gmail.com>
2024-02-27 17:52:38 +0100

    remove unwanted code

--- index.js
+++ index.js
@@ -49,15 +49,6 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("Mongo Error", err));
 
-// // connection with Mongoose
-// mongoose
-//   .connect("mongodb://localhost:27017/db", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch((err) => console.log("Mongo Error", err));
-
 // Import Mongoose models
 const User = Models.User;
 const Movie = Models.Movie;
